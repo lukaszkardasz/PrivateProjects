e2e9894028bb1ad89bceea480f0c06f7e26d8156
n2god <n2god@o2.pl>
2018-11-13 14:46:18 +0100

    another exercise with file read and write

--- JavaExercise/src/nauka/training/core/inOut/FIleExercise.java
+++ JavaExercise/src/nauka/training/core/inOut/FIleExercise.java
@@ -1,5 +1,7 @@
 package nauka.training.core.inOut;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -8,18 +10,43 @@ class FIleExercise {
 
 
     public static void main(String[] args) throws IOException {
-        String filePath = "D:\\java\\JavaExercise\\src\\nauka\\training\\core\\inOut\\File.txt";
+
+        // write to file
+        String filePathWrite = "D:\\java\\JavaExercise\\src\\nauka\\training\\core\\inOut\\File.txt";
         int number = 12345678;
         FileWriter fileWriter = null;
 
 
         try{
-            fileWriter = new FileWriter(filePath);
+            fileWriter = new FileWriter(filePathWrite);
             fileWriter.write(Integer.toString(number));
         } finally{
             if(fileWriter != null){
                 fileWriter.close();
             }
         }
+
+        //read from file
+
+        String filePathRead = "D:\\java\\JavaExercise\\src\\nauka\\training\\core\\inOut\\File2.txt";
+        int number2 = 0;
+        BufferedReader fileReader = null;
+
+        try{
+            fileReader = new BufferedReader( (new FileReader(filePathRead)));
+            String numberAsString = fileReader.readLine();
+            number2 = Integer.parseInt(numberAsString);
+            System.out.println(number2);
+        } finally {
+            if (fileReader != null){
+                fileReader.close();
+            }
+        }
+
+
+
+
+
+
     }
 }
