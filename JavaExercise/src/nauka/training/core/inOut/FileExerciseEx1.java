303af84b8757e98a73971753fce5a2407cea076a
n2god <n2god@o2.pl>
2018-11-13 22:29:13 +0100

    exercise 1 - sprawdzić dlaczego nie wczytuje kolejnej linii

--- JavaExercise/src/nauka/training/core/inOut/FileExerciseEx1.java
+++ JavaExercise/src/nauka/training/core/inOut/FileExerciseEx1.java
@@ -1,13 +1,34 @@
 package nauka.training.core.inOut;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 class FileExerciseEx1 {
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws IOException {
+        Scanner skaner = new Scanner(System.in);
+        String path = null;
+        System.out.println("Podaj ścieżkę i nazwę pliku: ");
+        FileWriter fileWriter = null;
+        path = skaner.nextLine();
+        if (path != null){
+            String linia = null;
+            fileWriter = new FileWriter(path);
+            do {
+                try{
+                    System.out.println("Podaj dane do zapisania w linii - \"-\" aby zakończyć!");
+                    linia = skaner.nextLine();
+                    fileWriter.write(linia);
+                    fileWriter.write(System.lineSeparator());
+                } catch (IOException ex){
+                    System.out.println("Błąd! Problem z dostepem do pliku!");
+                    fileWriter.close();
+                } finally {
+                    fileWriter.close();
+                    skaner.close();
+                }
+            } while (linia != "-");
+        }
 
 
     }
