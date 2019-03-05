1cd16bd0dee47b48d14cc7cb79ecdd783a9535b6
n2God <n2god@o2.pl>
2019-03-05 21:02:27 +0100

    exercise with agregation

---
+++ JavaExercise/src/nauka/Podstawy/Agregation.java
@@ -0,0 +1,37 @@
+package nauka.Podstawy;
+
+public class Agregation {
+
+    public static void main(String[] args) {
+
+        Human2 p = new Human2("Łukasz", new Adress("Krucza", 45));
+        System.out.println(p);
+    }
+
+}
+
+class Human2{
+    String name;
+    Adress adress;
+
+    public Human2(String name, Adress adress) {
+        this.name = name;
+        this.adress = adress;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ": " + adress.uliuca + " " + adress.nrDomu;
+    }
+}
+
+class Adress{
+    String uliuca;
+    int nrDomu;
+
+    public Adress(String uliuca, int nrDomu) {
+        this.uliuca = uliuca;
+        this.nrDomu = nrDomu;
+    }
+}
+
