aeac799c6643633a1a0d1c80a90bdefd1409802c
n2God <n2god@o2.pl>
2019-03-05 22:00:09 +0100

    change anonymous class to lambda expressions

--- JavaExercise/src/nauka/Podstawy/AnonymousClass/AnonymousClass.java
+++ JavaExercise/src/nauka/Podstawy/AnonymousClass/AnonymousClass.java
@@ -4,12 +4,12 @@ public class AnonymousClass {
 
     public static void main(String[] args) {
 
-        ActionAfterClick b = new ActionAfterClick() {
-            @Override
-            public void action() {
-                System.out.println("jestem z klasy anonimowej");
-            }
-        };
+        ActionAfterClick b = () -> System.out.println("jestem z klasy anonimowej");
+
+        ActionAfterClick2 b2 = (a, c) -> System.out.println(a + c);
+
+        b2.action2(2,22);
+
         b.action();
 
         Button button = new Button();
@@ -23,9 +23,16 @@ interface ActionAfterClick {
     void action();
 }
 
+interface ActionAfterClick2{
+    void action2(int a, int b);
+}
+
 class Button {
     void addAction(ActionAfterClick z) {
         z.action();
+    }
+    void addAction2 (int a, int b, ActionAfterClick2 z){
+        z.action2(a,b);
     }
 }
 
