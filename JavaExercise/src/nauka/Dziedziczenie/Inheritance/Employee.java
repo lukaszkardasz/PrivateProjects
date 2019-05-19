90104d56ef8cfaeca3a3bc371e04688cc5b3a98e
[n2god] <[n2god@o2.pl]>
2019-05-19 19:24:41 +0200

    Inheritance exercises

---
+++ JavaExercise/src/nauka/Dziedziczenie/Inheritance/Employee.java
@@ -0,0 +1,11 @@
+package nauka.Dziedziczenie.Inheritance;
+
+public class Employee {
+
+    String name;
+    String surname;
+    int birthYear;
+    short seniority;
+    String employeeInfo;
+
+}
