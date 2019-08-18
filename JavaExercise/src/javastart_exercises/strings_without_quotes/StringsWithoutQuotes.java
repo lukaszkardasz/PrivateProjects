8f528338b8c5876afea101c54d297c19d72ddef2
n2God <kardasz.lukasz@gmail.com>
2019-08-18 20:30:22 +0200

    string without any quotes

---
+++ JavaExercise/src/javastart_exercises/strings_without_quotes/StringsWithoutQuotes.java
@@ -0,0 +1,17 @@
+package javastart_exercises.strings_without_quotes;
+
+/**
+ * @author n2god on 18/08/2019
+ * @project PrivateProjects
+ */
+public class StringsWithoutQuotes {
+    public static void main(String[] args) {
+        char k = 75;
+        char o = 79;
+        char t = 84;
+
+        System.out.print(k);
+        System.out.print(o);
+        System.out.print(t);
+    }
+}
