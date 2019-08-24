a96cef6cf988fa3dde52d7b1fe3a2e1d06ac69f5
n2God <kardasz.lukasz@gmail.com>
2019-08-24 11:29:44 +0200

    init fizzBuzz

---
+++ JavaExercise/src/javastart_exercises/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,11 @@
+package javastart_exercises.FizzBuzz;
+
+/**
+ * @author n2god on 24/08/2019
+ * @project PrivateProjects
+ */
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+    }
+}
