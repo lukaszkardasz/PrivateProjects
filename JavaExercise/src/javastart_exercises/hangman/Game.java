b94122d4dd1b8152bc7366a0414c1032cb0d5bcc
n2God <kardasz.lukasz@gmail.com>
2019-08-31 15:39:41 +0200

    game main loop

--- JavaExercise/src/javastart_exercises/hangman/Game.java
+++ JavaExercise/src/javastart_exercises/hangman/Game.java
@@ -1,8 +1,27 @@
 package javastart_exercises.hangman;
 
+import java.util.Scanner;
+
 /**
  * @author n2god on 30/08/2019
  * @project PrivateProjects
  */
 public class Game {
+    private static final int NEXT_GAME = 1;
+    private static final int EXIT_GAME = 1;
+
+    private WordBase wb = new WordBase();
+    private Scanner scanner = new Scanner(System.in);
+
+    public void mainLoop(){
+        int option = NEXT_GAME;
+        while (option != EXIT_GAME){
+            play();
+            showOptions();
+            option = scanner.nextInt();
+            scanner.nextLine()
+        }
+        System.out.println("PaPa!");
+    }
+
 }
