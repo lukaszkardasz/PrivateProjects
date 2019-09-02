c9ba0e56fb029be46f98b34c0a68b740a8ff46f7
lukaszkardasz <kardasz.lukasz@gmail.com>
2019-09-03 00:04:45 +0200

    building class

--- JavaExercise/src/javastart_exercises/air_conditioner/Building.java
+++ JavaExercise/src/javastart_exercises/air_conditioner/Building.java
@@ -1,10 +1,61 @@
 package javastart_exercises.air_conditioner;
 
-public class Building extends Room {
+import java.util.Arrays;
+import java.util.Random;
 
-    public Building(double temperature, double cubature, boolean isConditionerEnabled) {
-        super(temperature, cubature, isConditionerEnabled);
+public class Building {
+    private Room[] rooms;
+
+    public Building(int numberOfRooms) {
+        rooms = new Room[numberOfRooms];
+        generateRooms();
     }
 
+    private void generateRooms() {
+        Random random = new Random();
+        for (int i = 0; i < rooms.length; i++) {
+            int currentTemperature = 20 + random.nextInt(15);
+            int targetTemperature = currentTemperature - random.nextInt(15);
+            int cubature = 20 + random.nextInt(30);
+            AirConditioner airConditioner = getRandomAirConditioner();
+            rooms[i] = new Room((i + 1), currentTemperature, targetTemperature, cubature, airConditioner, true);
+        }
+    }
 
+    private AirConditioner getRandomAirConditioner() {
+        Random random = new Random();
+        double value = random.nextDouble();
+        if (value > 0.5) {
+            return new BasicAirConditioner();
+        } else if (value < 0.5) {
+            return new AdvancedAirConditioner();
+        }
+
+        return null;
+    }
+
+    public boolean areAllRoomsCool() {
+        for (Room room : rooms) {
+            if (room.getCurrentTemperature() >= room.getTargetTemperature()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void enableAirConditioner() {
+        for (Room room : rooms) {
+            room.setConditionerEnabled(true);
+            room.cool();
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Room room : rooms) {
+            sb.append(room.toString()).append("\n");
+        }
+        return sb.toString();
+    }
 }
