04ab87dbfd83c1706c82b213e3b39420d1dd2ee2
lukaszkardasz <kardasz.lukasz@gmail.com>
2019-09-08 21:23:00 +0200

    finish and fix collect to map

--- JavaExercise/src/javastart_exercises/warehouse/Warehouse.java
+++ JavaExercise/src/javastart_exercises/warehouse/Warehouse.java
@@ -1,29 +1,42 @@
 package javastart_exercises.warehouse;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Warehouse {
     public static void main(String[] args) {
+        String filename = getFilename();
+        List<String> allLines = readFile(filename);
+
+        Map<String, Integer> map = allLines.stream()
+                .map(s -> s.split(";"))
+                .collect(Collectors.groupingBy(Map.Entry::getKey,
+                        Collectors.mapping(Map.Entry::getValue,
+                                Collectors.toList())));
+
+
+
+
+    }
+
+    private static String getFilename() {
         System.out.println("Podaj nazwê pliku: ");
         Scanner scanner = new Scanner(System.in);
-        String filename = scanner.nextLine();
-        List<String> allLines = new ArrayList<>();
-        File file = new File(filename);
+        return scanner.nextLine();
+    }
 
+    private static List<String> readFile(String filename){
         try {
-            allLines = Files.readAllLines(Paths.get(filename));
+            return Files.readAllLines(Paths.get(filename));
         } catch (IOException e) {
             System.err.println("No such file " + filename);
-            allLines = new ArrayList<>();
         }
-
+        return new ArrayList<>();
     }
 }
