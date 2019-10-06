27955add67e826f51e12747733667d68a4d62854
lukaszkardasz <kardasz.lukasz@gmail.com>
2019-10-06 11:38:24 +0200

    exercise - game machine create classes and exceptions

---
+++ JavaExercise/src/javastart_exercises/game_automat/GameMachine.java
@@ -0,0 +1,10 @@
+package javastart_exercises.game_automat;
+
+public class GameMachine {
+
+    Game buy(String gameTitle, double money){
+        Game boughtGame = null;
+
+        return boughtGame;
+    }
+}
