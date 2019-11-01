fc8ad8766049832316034ca9b50ec44a432fa3c5
lukaszkardasz <kardasz.lukasz@gmail.com>
2019-11-01 21:57:05 +0100

    add enum as option

---
+++ JavaExercise/src/javastart_phone_book/OPTIONS.java
@@ -0,0 +1,36 @@
+package javastart_phone_book;
+
+import java.util.NoSuchElementException;
+
+public enum OPTIONS {
+
+    ADD_CONTACT(0, "Dodaj nowy kontakt"),
+    SEARCH_BY_NAME(1, "Szukaj po nazwie"),
+    SEARCH_BY_NUMBER(2, "Szukaj po numerze telefonu"),
+    REMOVE_CONTACT(3, "Usuń kontakt"),
+    EXIT(4, "Wyjście z programu");
+
+    private final int shortcut;
+    private final String description;
+
+    OPTIONS(int shortcut, String description) {
+        this.shortcut = shortcut;
+        this.description = description;
+    }
+
+    public int getShortcut() {
+        return shortcut;
+    }
+
+    @Override
+    public String toString() {
+        return shortcut + " - " + description;
+    }
+
+    OPTIONS convertToOption (int option){
+        if(option < 0 || option >=OPTIONS.values().length){
+            throw new NoSuchElementException();
+        }
+        return OPTIONS.values()[option];
+    }
+}
