c6c4ffc47b6ce7b8764d93594252b12567482670
lukaszkardasz <kardasz.lukasz@gmail.com>
2019-11-01 23:21:14 +0100

    add other methods

--- JavaExercise/src/javastart_phone_book/PhoneBookController.java
+++ JavaExercise/src/javastart_phone_book/PhoneBookController.java
@@ -1,5 +1,6 @@
 package javastart_phone_book;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class PhoneBookController {
@@ -8,7 +9,16 @@ public class PhoneBookController {
     private Scanner sc = new Scanner(System.in);
 
     public void loop(){
-        showOptions();
+        OPTIONS option = null;
+        while(option != OPTIONS.EXIT){
+            showOptions();
+            try {
+                OPTIONS chooseOption = chooseOption();
+                executeOption(chooseOption);
+            } catch (NoSuchElementException e) {
+                System.out.println("Nieprawidłowa opcja, spróbuj jeszcze raz!");
+            }
+        }
     }
 
     private void showOptions(){
@@ -24,29 +34,35 @@ public class PhoneBookController {
         return OPTIONS.convertToOption(option);
     }
 
-    private void executeOption(OPTIONS option){
-
+    private void executeOption(OPTIONS options){
+        switch(options){
+            case ADD_CONTACT -> addContact();
+            case SEARCH_BY_NAME -> searchByName();
+            case SEARCH_BY_NUMBER -> searchByTelephone();
+            case REMOVE_CONTACT -> delete();
+            case EXIT -> close();
+        }
     }
 
     private void delete() {
-
+        //TODO add delete method;
     }
 
     private void searchByTelephone() {
-
+        //TODO add searchByTelephone method;
     }
 
     private void searchByName() {
-
+        //TODO add searchByName method;
     }
 
     private void addContact() {
-
+        //TODO add addContact method;
     }
 
     private void close() {
-
+        sc.close();
+        System.out.println("bye bye!");
+        System.exit(0);
     }
-
-
 }
