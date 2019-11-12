3c0367daa028d205a281356b3a8a696d6ee186e8
n2God <kardasz.lukasz@gmail.com>
2019-11-12 23:05:36 +0100

    FileLineCounter - XML problem

--- JavaExercise/src/javastart_exercises/files_line_counter/FileLineCounter.java
+++ JavaExercise/src/javastart_exercises/files_line_counter/FileLineCounter.java
@@ -1,6 +1,7 @@
 package javastart_exercises.files_line_counter;
 
 import java.io.*;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class FileLineCounter {
@@ -16,24 +17,42 @@ public class FileLineCounter {
         }
     }
 
-    private void run() throws IOException {
-        System.out.print("Podaj ścieżkę do folderu który ma zostać przeskanowany: ");
-
+    private String run() throws IOException {
         Scanner scanner = new Scanner(System.in);
-        String directoryLocation = scanner.nextLine();
 
-        File dir = new File(directoryLocation);
+        System.out.print("Podaj ścieżkę do folderu który ma zostać przeskanowany: ");
+        String directoryPath = scanner.nextLine();
+
+        System.out.println("Podaj ścieżkę do umieszczenia pliku XML: ");
+        String resultPath = scanner.nextLine();
+
+        File dir = new File(directoryPath);
         if (dir.exists()){
-            path = directoryLocation;
+            path = directoryPath;
             processDirectory(dir);
             System.out.println("W sumie folder " + path + " ma " + totalCounter + " linii.");
         } else {
             System.out.println("Wskazany folder nie istnieje!");
         }
+
+        String xmlFilename = resultPath + File.separator + "LineCounter.xml";
+        File resultFile = new File(xmlFilename);
+        if (!resultFile.exists()){
+            try {
+                resultFile.createNewFile();
+                System.out.println(xmlFilename);
+            } catch (IOException e) {
+                System.out.println("Nie udało się utworzyć pliku, ścieżka: " + xmlFilename + " jest nieprawidłowa!");
+            }
+        } else {
+            System.out.println("Plik " + resultFile.getAbsolutePath() + " już istnieje.");
+        }
+        scanner.close();
+        return xmlFilename;
     }
 
     private void processDirectory(File dir) throws IOException {
-        for (File file : dir.listFiles()) {
+        for (File file : Objects.requireNonNull(dir.listFiles())) {
             if(file.isDirectory()){
                 processDirectory(file);
             } else {
@@ -41,7 +60,24 @@ public class FileLineCounter {
                 totalCounter += lines;
 
                 String replacedName = file.getAbsolutePath().replace(path, "");
-                System.out.println(replacedName + " --> " + lines);
+
+                //System.out.println(replacedName + " --> " + lines);
+                //TODO dodać implementację dodawania do XML nazwy pliku, folderu i liczby plików
+                //problem w tym, że do metody rekurencyjnej nie mogę dodać ko0lejnego parametru, z nazwą podawanego pliku
+                //nie mogę tego zrobić statycznie
+/*                try (
+                        var fileWriter = new FileWriter(xmlFilename);
+                        var writer = new BufferedWriter(fileWriter);
+                ) {
+                    writer.write("Bolek");
+                    writer.newLine();
+                    writer.write("Lolek");
+                    writer.newLine();
+                    writer.write("Karolek");
+                } catch (IOException e) {
+                    System.err.println("Nie udało się zapisać pliku " + fileName);
+                }*/
+
             }
         }
     }
